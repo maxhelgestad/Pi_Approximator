0b13f790287b77cef491b2b6de8a1946b5c50e88
maxhelgestad <56936702+maxhelgestad@users.noreply.github.com>
2020-10-01 15:48:33 -0500

    Add files via upload

---
+++ DoublePoint.java
@@ -0,0 +1,17 @@
+public class DoublePoint{
+  double x;
+  double y;
+  
+  public DoublePoint(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+  
+  public double getX(){
+    return this.x;
+  }
+  
+  public double getY(){
+    return this.y;
+  }
+}
